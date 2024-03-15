3b552fc24a1b762bb6f1974007ebd79e0cea8c33
huzhe46 <hu-zhe@outlook.com>
2024-03-15 17:28:58 +0800

    style(cell): jdesign style cell

--- src/packages/cell/demos/h5/demo3.tsx
+++ src/packages/cell/demos/h5/demo3.tsx
@@ -1,23 +1,37 @@
 import React from 'react'
 import { Cell } from '@nutui/nutui-react'
-import { User } from '@nutui/icons-react'
+import { Apps, User, Warning } from '@nutui/icons-react'
 
 const Demo3 = () => {
   return (
-    <Cell
-      title={
-        <div style={{ display: 'inline-flex', alignItems: 'center' }}>
-          <User />
-          <span style={{ marginLeft: '5px' }}>我是标题</span>
-        </div>
-      }
-      description={
-        <span>
-          我是描述<b style={{ color: 'red' }}>1</b>
-        </span>
-      }
-      extra="描述文字"
-    />
+    <Cell.Group>
+      <Cell
+        title={
+          <div style={{ display: 'inline-flex', alignItems: 'center' }}>
+            <User />
+            <span style={{ marginLeft: '5px' }}>我是标题</span>
+          </div>
+        }
+        description={
+          <span>
+            我是描述<b style={{ color: 'red' }}>1</b>
+          </span>
+        }
+        extra="描述文字"
+      />
+      <Cell
+        title={
+          <div style={{ display: 'inline-flex', alignItems: 'center' }}>
+            <Apps />
+            <span style={{ marginLeft: '5px', marginRight: '2px' }}>
+              我是标题
+            </span>
+            <Warning width={12} height={12} />
+          </div>
+        }
+        extra="描述文字"
+      />
+    </Cell.Group>
   )
 }
 export default Demo3
